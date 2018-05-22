3fe827fd3e3028967676cce62db2272af5f06a72
Kyle Hommes <kylehommes@yahoo.com>
2018-05-21 19:20:45 -0600

    Update

---
+++ Clean.R
@@ -0,0 +1,92 @@
+mhatweet <- twListToDF(mentalhealthawarenesstweets)
+anxtweet <- twListToDF(anxietytweets)
+deptweet <- twListToDF(depressiontweets)
+ptsdtweet <- twListToDF(ptsdtweets)
+suitweet <- twListToDF(suicidetweets)
+totaltweet <- bind_rows(mhatweet %>% mutate(hashtag = "MHA"),
+                        anxtweet %>% mutate(hashtag = "Anxiety"), deptweet %>%
+                          mutate(hashtag = "Depression"), ptsdtweet %>% 
+                          mutate(hashtag = "PTSD"), suitweet %>% 
+                          mutate(hashtag = "Suicide"))
+
+# Cleaned the tweets for tidy analysis with code adapted from 
+# https://www.tidytextmining.com
+replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+tidytweet <- totaltweet %>%
+  group_by(hashtag) %>% 
+  dplyr::mutate(linenumber = row_number()) %>%
+  ungroup() %>% filter(!str_detect(text, "^RT")) %>%
+  mutate(text = str_replace_all(text, replace_reg, "")) %>%
+  unnest_tokens(word, text, token = "regex", 
+                pattern = unnest_reg) %>%
+  filter(!word %in% stop_words$word, str_detect(word, "[a-z]"))
+
+# Cleaning process for TDM creating using tm package
+ctrl <- list(removePunctuation= list(preserve_intra_word_dashes = T), tolower= T, stopwords= c(stopwords(kind = "en"), removeNumbers= T))
+mhatxt <- sapply(mentalhealthawarenesstweets, 
+                 function(x) x$getText())
+mhatxt <- iconv(mhatxt, "latin1", "ASCII", sub = "")
+mhatxt <- gsub("http(s?)([^ ]*)", " ", mhatxt, ignore.case = T)
+mhatxt <- gsub("&amp", "and", mhatxt)
+mhacorpus <- VCorpus(VectorSource(mhatxt))
+mhatdm <- TermDocumentMatrix(x= mhacorpus, control= ctrl)
+mhadtm <- DocumentTermMatrix(x= mhacorpus, control= ctrl)
+mhafreq <- sort(rowSums(as.matrix(mhatdm)), decreasing = T)
+mhafreq.df <- data.frame(word = names(mhafreq), 
+                         freq = mhafreq, stringsAsFactors = F, row.names = NULL)
+stxt <- sapply(suicidetweets, 
+               function(x) x$getText())
+stxt <- iconv(stxt, "latin1", "ASCII", sub = "")
+stxt <- gsub("http(s?)([^ ]*)", " ", stxt, ignore.case = T)
+stxt <- gsub("&amp", "and", stxt)
+scorpus <- VCorpus(VectorSource(stxt))
+stdm <- TermDocumentMatrix(x= scorpus, control= ctrl)
+sdtm <- DocumentTermMatrix(x= scorpus, control= ctrl)
+sfreq <- sort(rowSums(as.matrix(stdm)), decreasing = T)
+sfreq.df <- data.frame(word = names(sfreq), 
+                       freq = sfreq, stringsAsFactors = F, row.names = NULL)
+atxt <- sapply(anxietytweets, 
+               function(x) x$getText())
+atxt <- iconv(atxt, "latin1", "ASCII", sub = "")
+atxt <- gsub("http(s?)([^ ]*)", " ", atxt, ignore.case = T)
+atxt <- gsub("&amp", "and", atxt)
+acorpus <- VCorpus(VectorSource(atxt))
+atdm <- TermDocumentMatrix(x= acorpus, control= ctrl)
+adtm <- DocumentTermMatrix(x= acorpus, control= ctrl)
+afreq <- sort(rowSums(as.matrix(atdm)), decreasing = T)
+afreq.df <- data.frame(word = names(afreq), 
+                       freq = afreq, stringsAsFactors = F, row.names = NULL)
+dtxt <- sapply(depressiontweets, 
+               function(x) x$getText())
+dtxt <- iconv(dtxt, "latin1", "ASCII", sub = "")
+dtxt <- gsub("http(s?)([^ ]*)", " ", dtxt, ignore.case = T)
+dtxt <- gsub("&amp", "and", dtxt)
+dcorpus <- VCorpus(VectorSource(dtxt))
+dtdm <- TermDocumentMatrix(x= dcorpus, control= ctrl)
+ddtm <- DocumentTermMatrix(x= dcorpus, control= ctrl)
+dfreq <- sort(rowSums(as.matrix(dtdm)), decreasing = T)
+dfreq.df <- data.frame(word = names(dfreq), 
+                       freq = dfreq, stringsAsFactors = F, row.names = NULL)
+ptxt <- sapply(ptsdtweets, 
+               function(x) x$getText())
+ptxt <- iconv(ptxt, "latin1", "ASCII", sub = "")
+ptxt <- gsub("http(s?)([^ ]*)", " ", ptxt, ignore.case = T)
+ptxt <- gsub("&amp", "and", ptxt)
+pcorpus <- VCorpus(VectorSource(ptxt))
+ptdm <- TermDocumentMatrix(x= pcorpus, control= ctrl)
+pdtm <- DocumentTermMatrix(x= pcorpus, control= ctrl)
+pfreq <- sort(rowSums(as.matrix(ptdm)), decreasing = T)
+pfreq.df <- data.frame(word = names(pfreq), 
+                       freq = pfreq, stringsAsFactors = F, row.names = NULL)
+tottxt <- totaltweet$text
+tottxt <- iconv(tottxt, "latin1", "ASCII", sub = "")
+tottxt <- gsub("http(s?)([^ ]*)", " ", tottxt, ignore.case = T)
+tottxt <- gsub("&amp", "and", tottxt)
+totcorpus <- VCorpus(VectorSource(tottxt))
+totcorpus <- tm_map(totcorpus, PlainTextDocument)
+tottdm <- TermDocumentMatrix(x= totcorpus, control= ctrl)
+totdtm <- DocumentTermMatrix(x= totcorpus, control= ctrl)
+totfreq <- sort(rowSums(as.matrix(tottdm)), decreasing = T)
+totfreq.df <- data.frame(word = names(totfreq), 
+                         freq = totfreq, stringsAsFactors = F, row.names = NULL)
--- Gecoding.R
+++ Gecoding.R
@@ -8,3 +8,21 @@ userframe.dt$location <-
   userframe.dt$location[!userframe.dt$location %in% ""]
 location <- geocode(userframe.dt$location[!userframe.dt$location
   %in% ""])
+
+register_google(key = google.key)
+mhtweets.df <- twListToDF(mentalhealthawarenesstweets)
+userinfo <- lookupUsers(mhtweets.df$screenName)
+userframe <- twListToDF(userinfo)
+userframe.dt <- data.table(userframe)
+userframe.dt$location <-
+  userframe.dt$location[!userframe.dt$location %in% ""]
+userlocation <- geocode(userframe.dt$location[1:2000])
+userlocation1 <- geocode(userframe.dt$location[2001:4000])
+userlocation2 <- geocode(userframe.dt$location[4001:6000])
+userlocation3 <- geocode(userframe.dt$location[6001:8000])
+userlocation4 <- geocode(userframe.dt$location[8001:10000])
+userlocation5 <- geocode(userframe.dt$location[10001:12000])
+userlocation6 <- geocode(userframe.dt$location[12001:14000])
+userlocation7 <- geocode(userframe.dt$location[14001:16000])
+userlocation8 <- geocode(userframe.dt$location[16001:17066])
+location <- bind_rows(userlocation,userlocation1)
